b22b89af770d7ff11d511d18893892bb91268dcd
Nandha23311 <nanda23311@gmai.com>
2023-12-21 20:36:15 +0530

    feat: version v1.0.1

--- gnosql_client.go
+++ gnosql_client.go
@@ -17,10 +17,12 @@ var EndpointsMap = Endpoints{
 }
 
 type Client struct {
-	URI string
+	URI string // Ex: http://localhost:5454
 	DB  map[string]*Database
 }
 
+// Create new GnoSQL client, 
+// URI string  Ex: http://localhost:5454
 func Connect(URI string) *Client {
 	return &Client{
 		URI: URI,
